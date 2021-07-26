4a9a715097615bde2ad7143357ca041a47272c2e
KevinAndresG <3348@holbertonschool.com>
2021-07-26 11:47:13 -0500

    creating the main file and the file eith the main function

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+
+
+/**
+ * _printf - our own printf.
+ *
+ * @format: a char pointer named format.
+ *
+ * Return: an int
+ */
+
+int _printf(const char *format, ...)
+{
+return (0);
+}
